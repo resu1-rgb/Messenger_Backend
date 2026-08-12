df1ff839c75b0a1244d2d1f51c0a56b49e6e3a24
RESUL <resu1.pv@gmail.com>
2026-08-12 21:37:31 +0300

    Add database

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+engine = create_engine("postgresql://resul@localhost/messages_backend")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
